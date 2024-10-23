45ffc77052f48a45334cf1be0d7865efcd8ec49c
ramani <ramani@gmail.com>
2024-10-23 04:31:27 +0000

    docker image

--- kc-internal/kc-internal-vnc-start.sh
+++
@@ -1,5 +0,0 @@
-vncserver -localhost no -SecurityTypes None --I-KNOW-THIS-IS-INSECURE
-websockify -D --web=/usr/share/novnc 40201 localhost:5901
-mkdir -p /root/Desktop
-cp /opt/kc-internal/Desktop/* /root/Desktop/ || true
-chmod a+x /root/Desktop/* || true
